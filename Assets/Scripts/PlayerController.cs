493b7c21a9ff68cef2fd01c407916e4cff58d03a
neocotic <mercer.alasdair@gmail.com>
2025-03-04 10:54:35 +0000

    add mouse control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Range(1, 100)]
+    public float mouseSensitivity = 25f;
+    [Range(1, 100)]
     public float moveSpeed = 5f;
 
     void Update()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
-        var movement = new Vector2(horizontalInput, 0f) * (moveSpeed * Time.deltaTime);
+        var mouseXInput = Input.GetAxis("Mouse X") * mouseSensitivity;
+        var input = horizontalInput != 0f ? horizontalInput : mouseXInput;
+        var movement = new Vector2(input, 0f) * (moveSpeed * Time.deltaTime);
 
         transform.Translate(movement);
     }
